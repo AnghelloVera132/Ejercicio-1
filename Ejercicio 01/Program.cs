08918bb5f1ef0fca14e8f8e81045a1f4ce0a7fa9
User <User@DESKTOP-411OTHH>
2023-09-21 21:04:50 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio 01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int factor1 = 0;
+            int factor2 = 0;
+            int resoult = 0;
+            
+            for (int i = 0; i < factor1; i++) 
+            { 
+                resoult = resoult + factor2;
+            }
+            Console.WriteLine("El resultado es: "+ resoult);
+            Console.ReadKey();
+        }
+    }
+}
